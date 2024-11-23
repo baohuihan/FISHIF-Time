7939a8cb04918a6890f1c1198b99bdda66597f6e
baohuihan <baohuihan@outlook.com>
2024-11-23 20:30:49 +0800

    Delete Analysis directory

--- Analysis/hb analysis/unsteady state model/fit/ModuleFitTimeOld2.m
+++
@@ -1,7 +0,0 @@
-function [KannealG,MeanExpFitG,PiniG]=ModuleFitTime(RnaSignal,TimeLabel,OutFolder,InheritSwitch,Model)
-%%% A program to fit the experiment data by model
-KannealG=[];MeanExpFitG=[];PiniG=[];
-switch Model
-    case 'TwoState'
-        [KannealG,MeanExpFitG,PiniG]=ModuleFitTime_TwoState(RnaSignal,TimeLabel,OutFolder,InheritSwitch);
-end
